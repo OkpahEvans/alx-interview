ce21cde2f5edf832ca7483d7294a5367b685b928
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2023-07-06 19:45:58 -0700

    Update 0-lockboxes.py

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-
-""" A module for lockboxes task"""
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened """
-    unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
             continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
