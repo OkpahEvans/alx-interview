85735a9a97b45a445e741fbfe1fa90087b36fa12
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-01 14:10:25 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,29 +5,29 @@ Change comes from within
 
 
 def makeChange(coins, total):
-        """
-            Given a pile of coins of different values, determine the fewest
-                number of coins needed to meet a given amount total.
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
 
-                    """
-                        if total <= 0:
-                                    return 0
+    """
+    if total <= 0:
+        return 0
 
-                                    placeholder = total + 1
+    placeholder = total + 1
 
-                                        memo = {0: 0}
+    memo = {0: 0}
 
-                                            for i in range(1, total + 1):
-                                                        memo[i] = placeholder
+    for i in range(1, total + 1):
+        memo[i] = placeholder
 
-                                                                for coin in coins:
-                                                                                current = i - coin
-                                                                                            if current < 0:
-                                                                                                                continue
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
 
-                                                                                                                        memo[i] = min(memo[current] + 1, memo[i])
+            memo[i] = min(memo[current] + 1, memo[i])
 
-                                                                                                                            if memo[total] == total + 1:
-                                                                                                                                        return -1
+    if memo[total] == total + 1:
+        return -1
 
-                                                                                                                                        return memo[total]
+    return memo[total]
