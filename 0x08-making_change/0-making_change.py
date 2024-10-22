430d72c24d2893fa5a2455e4dfd0b76aa61613f1
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 07:06:32 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""
-Change comes from within
+"""Change making module.
 """
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
     if total <= 0:
         return 0
-
-    placeholder = total + 1
-
-    memo = {0: 0}
-
-    for i in range(1, total + 1):
-        memo[i] = placeholder
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            memo[i] = min(memo[current] + 1, memo[i])
-
-    if memo[total] == total + 1:
-        return -1
-
-    return memo[total]
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
